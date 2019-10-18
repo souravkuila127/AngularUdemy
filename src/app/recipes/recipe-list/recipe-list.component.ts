2b637b54317ff548706328887505c404df27dace
Sourav Kuila <souravkuila127@gmail.com>
2019-10-18 23:51:20 +0530

    Section 3 Complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A test recipe','This is simply a test','https://images-na.ssl-images-amazon.com/images/I/81-yKbVND-L._SY355_.png')
+  ];
 
   constructor() { }
 
